dbc643c8481c0f4fc24a2feea024c8a2fa085d9e
JBlimp <65824519+JBlimp@users.noreply.github.com>
2023-11-03 14:55:46 +0900

    23.11.3.

---
+++ carrotmarket_scrollview /carrotmarket/MyTestTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  MyTestTableViewCell.swift
+//  carrotmarket
+//
+//  Created by junseok on 10/10/23.
+//
+
+import UIKit
+
+class MyTestTableViewCell: UITableViewCell {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
