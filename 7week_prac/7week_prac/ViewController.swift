8da1674ab793f6bc7987560804b6410f802d8d04
JBlimp <65824519+JBlimp@users.noreply.github.com>
2023-11-22 12:01:29 +0900

    11.22.

---
+++ 7week_prac/7week_prac/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  7week_prac
+//
+//  Created by junseok on 11/22/23.
+//
+
+import UIKit
+import KakaoSDKUser
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var kakaoLoginButton: UIButton!
+    
+    @IBAction func kakaoLoginButtonTouchUpInside(_ sender: Any) {
+        if (UserApi.isKakaoTalkLoginAvailable()) {
+            
+            //카톡 설치되어있으면 -> 카톡으로 로그인
+            UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("카카오 톡으로 로그인 성공")
+                    
+                    _ = oauthToken
+                    /// 로그인 관련 메소드 추가
+                }
+            }
+        } else {
+
+            // 카톡 없으면 -> 계정으로 로그인
+            UserApi.shared.loginWithKakaoAccount { (oauthToken, error) in
+                if let error = error {
+                    print(error)
+                } else {
+                    print("카카오 계정으로 로그인 성공")
+                    
+                    _ = oauthToken
+                    // 관련 메소드 추가
+                }
+            }
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
